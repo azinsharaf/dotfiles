db31da05318058d19ce8af594ca5e1cb3b687d7a
azinsharaf <azin.sharaf@icloud.com>
2024-02-16 10:16:47 -0800

    rainmeter catppucin skin with komorebi widget

---
+++ Documents/Rainmeter/Skins/catppuccin/@resources/inc/calendar.ps1
@@ -0,0 +1,24 @@
+function setInline {
+    $RmAPI.Bang("!setoptiongroup date inlinesetting none")
+    $inline=$RmAPI.VariableStr("iSetting")
+    $sDate=$RmAPI.Measure("setDate")
+    $mEnd=$RmAPI.Measure("mEnd")
+    $mMonth=$RmAPI.Variable("mMonth")
+    for($d=0; $d -lt 43; $d++) {
+        $date=$RmAPI.VariableStr("$d")
+        $eText=$RmAPI.VariableStr("$date" + ".$mMonth")
+        if($null -notMatch [regex]::escape($eText)) {
+            $RmAPI.Bang("!setoption $d inlinesetting $inline")
+            if((($d-$sDate) -ge $mEnd) -or (($d-$sDate) -gt 31) -or (($d-$sDate) -lt 1)) {
+                $RmAPI.Bang("!setoption $d inlinesetting none")
+            }
+        }
+    }
+}
+
+function deleteEmptyVars {
+    $varPath=$RmAPI.VariableStr("@") + "inc\var.inc"
+    set-Content $varPath (get-Content $varPath | select-String -pattern "^(\d|\d\d)\.(\d|\d\d)=$" -notMatch)
+}
+
+deleteEmptyVars
