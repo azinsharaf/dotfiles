ad964c1bd213b035cee307a5782bdac1e0e996ed
azinsharaf <azin.sharaf@icloud.com>
2024-03-30 18:22:15 -0700

    nvim, harpoon plugin

---
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,86 @@
+local prefix = "<leader><leader>"
+local term_string = vim.fn.exists("$TMUX") == 1 and "tmux" or "term"
+local maps = { n = {} }
+local icon = vim.g.icons_enabled and "󱡀 " or ""
+maps.n[prefix] = { desc = icon .. "Harpoon" }
+-- require("astronvim.utils").set_mappings(maps)
+return {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		keys = {
+			{
+				prefix .. "a",
+				function()
+					require("harpoon"):list():append()
+				end,
+				desc = "Add file",
+			},
+			{
+				prefix .. "e",
+				function()
+					require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+				end,
+				desc = "Toggle quick menu",
+			},
+			{
+				"<TAB>",
+				function()
+					require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+				end,
+				desc = "Toggle quick menu",
+			},
+			{
+				"<C-x>",
+				function()
+					vim.ui.input({ prompt = "Harpoon mark index: " }, function(input)
+						local num = tonumber(input)
+						if num then
+							require("harpoon"):list():select(num)
+						end
+					end)
+				end,
+				desc = "Goto index of mark",
+			},
+			{
+				"<C-p>",
+				function()
+					require("harpoon"):list():prev()
+				end,
+				desc = "Goto previous mark",
+			},
+			{
+				"<C-n>",
+				function()
+					require("harpoon"):list():next()
+				end,
+				desc = "Goto next mark",
+			},
+			{ prefix .. "m", "<cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" },
+			{ "<leader>" .. "f" .. "h", "<cmd>Telescope harpoon marks<CR>", desc = "harpoon marks in Telescope" },
+			{
+				prefix .. "t",
+				function()
+					vim.ui.input({ prompt = term_string .. " window number: " }, function(input)
+						local num = tonumber(input)
+						if num then
+							require("harpoon").term.gotoTerminal(num)
+						end
+					end)
+				end,
+				desc = "Go to " .. term_string .. " window",
+			},
+		},
+		opts = {},
+	},
+	{
+		"catppuccin/nvim",
+		optional = true,
+		-- @type CatppuccinOptions
+		opts = { integrations = { harpoon = true } },
+	},
+}
