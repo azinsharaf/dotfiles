2700eaba21272fb21925abe4d4688d73e6d667c3
azinsharaf <azin.sharaf@icloud.com>
2024-04-19 09:26:14 -0700

    neovim, adding more keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,14 +1,22 @@
-local telescope = require("telescope")
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
+local telescope = require("telescope")
 telescope.load_extension("chezmoi")
 
 -- general
+keymap("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+-- keymap("n", "<leader>nh", ":noh<CR>", { desc = "No Highlight" })
 -- keymap("n", "<leader>w", ":w<CR>", { desc = "Write" })
 -- keymap("n", "<leader>q", "<cmd>confirm q<cr>", { desc = "Quit" })
 -- keymap("n", "<leader>Q", "<cmd>confirm qall<cr>", { desc = "Quit All" })
 -- keymap("n", "<leader>n", "<cmd>enew<cr>", { desc = "New File" })
+
+-- window management
+keymap("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 -- keep the cursor in center (needs to be tested)
 keymap("n", "n", "nzz", opts)
