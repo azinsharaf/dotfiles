448cc850916d67d2a3d1f351c74b6eb163cfc1e9
Azin Sharaf <azin.sharaf@icloud.com>
2024-04-19 14:18:46 -0700

    neovim, lualine

--- dot_config/nvim/lua/plugins/lualine-nvim.lua
+++ dot_config/nvim/lua/plugins/lualine-nvim.lua
@@ -23,7 +23,7 @@ local config = function()
 			lualine_b = { "branch", "diff", "diagnostics" },
 			lualine_c = { "filename" },
 			lualine_x = { "encoding", "fileformat", "filetype" },
-			lualine_y = { "progress" },
+			-- lualine_y = { "progress" },
 			lualine_z = { "location" },
 		},
 		inactive_sections = {
