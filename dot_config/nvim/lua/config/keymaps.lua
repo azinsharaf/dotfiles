63611ec03f6ba6dfb5fa40f6ab7149220d76390d
Azin Sharaf <azin.sharaf@icloud.com>
2024-04-20 09:28:15 -0700

    neovim, keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -18,6 +18,14 @@ keymap("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- s
 keymap("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+keymap("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+keymap("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+keymap("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+
 -- keep the cursor in center (needs to be tested)
 keymap("n", "n", "nzz", opts)
 keymap("n", "N", "Nzz", opts)
@@ -30,46 +38,3 @@ keymap("v", "J", ":m '>+1<cr>gv=gv")
 keymap("v", "K", ":m '<-2<cr>gv=gv")
 
 keymap("n", "<RightMouse>", "<cmd>:popup PopUp<CR>")
-local wk = require("which-key")
-
-wk.register({
-
-	-- file related
-	-- f = {
-	-- 	name = "File",
-	-- 	f = { "<cmd>Telescope find_files<cr>", "Find Files" },
-	-- 	c = { telescope.extensions.chezmoi.find_files, "Find chezmoi files" },
-	-- 	g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
-	-- 	b = { "<cmd>Telescope buffers<cr>", "Buffers" },
-	-- 	h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
-	-- 	o = { "<cmd>Telescope oldfiles<cr>", "Old Files" },
-	-- 	r = { "<cmd>Telescope registers<cr>", "Registers" },
-	-- 	s = { "<cmd>Telescope spell_suggest<cr>", "Spell Suggestions" },
-	-- 	k = { "<cmd>Telescope keymaps<cr>", "Key Maps" },
-	-- 	x = { "<cmd>Telescope file_browser hidden=true<cr>", "File Browser" },
-	-- },
-
-	-- git related using telescope
-	-- g = {
-	-- 	name = "Git",
-	-- 	s = { "<cmd>Telescope git_status<cr>", "Git Status" },
-	-- 	c = { "<cmd>Telescope git_commits<cr>", "Git Commits" },
-	-- 	b = { "<cmd>Telescope git_branches<cr>", "Git Branches" },
-	-- 	f = { "<cmd>Telescope git_files<cr>", "Git Files" },
-	-- },
-
-	-- package manager and mason related
-	p = {
-		name = "Package",
-		p = { "<cmd>Lazy<cr>", "Lazy Package Manager" },
-		m = { "<cmd>Mason<cr>", "Mason" },
-	},
-
-	-- toggles
-	t = {
-		name = "Toggle",
-		k = { "<cmd>Screenkey<cr>", "Screenkey" },
-	},
-
-	c = { "<Plug>(comment_toggle_linewise_current)", "Comment" },
-}, { prefix = "<leader>" })
--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -1,9 +1,20 @@
 return {
 	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
+	event = "VimEnter", -- VeryLazy",
+	config = function()
 		vim.o.timeout = true
 		vim.o.timeoutlen = 500
+		require("which-key").setup()
+
+		-- Document existing key chains
+		require("which-key").register({
+			["<leader>e"] = { name = "[e]xplorer", _ = "which_key_ignore" },
+			["<leader>f"] = { name = "[f]ile", _ = "which_key_ignore" },
+			["<leader>h"] = { name = "git [h]unks", _ = "which_key_ignore" },
+			["<leader>p"] = { name = "[p]ackages", _ = "which_key_ignore" },
+			["<leader>s"] = { name = "[s]plit", _ = "which_key_ignore" },
+			["<leader>w"] = { name = "[w]orkspaces", _ = "which_key_ignore" },
+		})
 	end,
 	opts = {
 		-- your configuration comes here
