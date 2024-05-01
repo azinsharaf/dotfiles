ebff847938abbb0f7f94c37b2de793faeaa9bad5
Azin Sharaf <azin.sharaf@icloud.com>
2024-04-30 22:29:45 -0700

    neovim, adding keymaps for obsidian

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -11,14 +11,10 @@ return {
 			["<leader>e"] = { name = "[e]xplorer", _ = "which_key_ignore" },
 			["<leader>f"] = { name = "[f]ile", _ = "which_key_ignore" },
 			["<leader>h"] = { name = "git [h]unks", _ = "which_key_ignore" },
+			["<leader>o"] = { name = "[o]bsidian", _ = "which_key_ignore" },
 			["<leader>p"] = { name = "[p]ackages", _ = "which_key_ignore" },
 			["<leader>s"] = { name = "[s]plit", _ = "which_key_ignore" },
 			["<leader>w"] = { name = "[w]orkspaces", _ = "which_key_ignore" },
 		})
 	end,
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
 }
