8817be1468f341f43b1426250ee6e5249562f575
Azin Sharaf <azin.sharaf@icloud.com>
2024-07-26 16:30:16 -0700

    nvim, which key

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -1,53 +1,54 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-    },
-    keys = {
-        {
-            "<leader>?",
-            function()
-                require("which-key").show({ global = false })
-            end,
-            desc = "Buffer Local Keymaps (which-key)",
-        },
-        {
-            "<leader>e",
-            function()
-                require("which-key").show({ global = false })
-            end,
-            desc = "Explorer",
-        },
-        {
-            "<leader>f",
-            function()
-                require("which-key").show({ global = false })
-            end,
-            desc = "Find",
-        },
-        {
-            "<leader>h",
-            function()
-                require("which-key").show({ global = false })
-            end,
-            desc = "Git Hunk",
-        },
-        {
-            "<leader>o",
-            function()
-                require("which-key").show({ global = false })
-            end,
-            desc = "Obsidian",
-        },
-        {
-            "<leader>s",
-            function()
-                require("which-key").show({ global = false })
-            end,
-            desc = "Split",
-        },
-    },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		delay = 0,
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+		{
+			"<leader>e",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Explorer",
+		},
+		{
+			"<leader>f",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Find",
+		},
+		{
+			"<leader>h",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Git Hunk",
+		},
+		{
+			"<leader>o",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Obsidian",
+		},
+		{
+			"<leader>s",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Split",
+		},
+	},
 }
