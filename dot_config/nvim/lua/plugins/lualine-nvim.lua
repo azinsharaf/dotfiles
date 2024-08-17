7c817e72a2aff4bdaa1a3612d9811eddfeade3ad
Azin Sharaf <azin.sharaf@icloud.com>
2024-08-16 23:43:07 -0700

    nvim, lualine, adding codeium status

--- dot_config/nvim/lua/plugins/lualine-nvim.lua
+++ dot_config/nvim/lua/plugins/lualine-nvim.lua
@@ -1,49 +1,55 @@
+-- Function to get Codeium status
+local function get_codeium_status()
+    return vim.api.nvim_call_function("codeium#GetStatusString", {})
+end
+
 local config = function()
-	require("lualine").setup({
-		options = {
-			icons_enabled = true,
-			theme = "auto",
-			component_separators = { left = "|", right = "|" },
-			section_separators = { left = "", right = "" },
-			disabled_filetypes = {
-				statusline = {},
-				winbar = {},
-			},
-			ignore_focus = {},
-			always_divide_middle = true,
-			globalstatus = false,
-			refresh = {
-				statusline = 1000,
-				tabline = 1000,
-				winbar = 1000,
-			},
-		},
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch", "diff", "diagnostics" },
-			lualine_c = { "filename" },
-			lualine_x = { "encoding", "fileformat", "filetype" },
-			-- lualine_y = { "progress" },
-			lualine_z = { "location" },
-		},
-		inactive_sections = {
-			lualine_a = {},
-			lualine_b = {},
-			lualine_c = { "filename" },
-			lualine_x = { "location" },
-			lualine_y = {},
-			lualine_z = {},
-		},
-		tabline = {},
-		winbar = {},
-		inactive_winbar = {},
-		extensions = {},
-	})
+    require("lualine").setup({
+        options = {
+            icons_enabled = true,
+            theme = "auto",
+            component_separators = { left = "|", right = "|" },
+            section_separators = { left = "", right = "" },
+            disabled_filetypes = {
+                statusline = {},
+                winbar = {},
+            },
+            ignore_focus = {},
+            always_divide_middle = true,
+            globalstatus = false,
+            refresh = {
+                statusline = 1000,
+                tabline = 1000,
+                winbar = 1000,
+            },
+        },
+        sections = {
+            lualine_a = { "mode" },
+            lualine_b = { "branch", "diff", "diagnostics" },
+            lualine_c = { "filename" },
+
+            lualine_x = { get_codeium_status, "encoding", "fileformat", "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = { "location" },
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { "filename" },
+            lualine_x = { "location" },
+            lualine_y = {},
+            lualine_z = {},
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
+    })
 end
 
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	lazy = false,
-	config = config,
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    config = config,
 }
