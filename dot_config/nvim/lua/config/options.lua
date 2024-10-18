304004bbc4e60d3b744cb85f0efb4e55e0d2e912
Azin Sharaf <azin.sharaf@icloud.com>
2024-10-18 08:43:09 -0700

    nvim, option file

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,33 +3,32 @@ vim.cmd("let g:netrw_lifestyle = 3")
 local opt = vim.opt
 
 local powershell_options = {
-    shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-    shellcmdflag =
-    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-    shellquote = "",
-    shellxquote = "",
+	shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+	shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+	shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+	shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+	shellquote = "",
+	shellxquote = "",
 }
 
 for option, value in pairs(powershell_options) do
-    opt[option] = value
+	opt[option] = value
 end
 
 -- Tab / Indentation
-opt.tabstop = 4       -- 4 spaces for tabs
-opt.shiftwidth = 4    --4 spaces for indent width
+opt.tabstop = 4 -- 4 spaces for tabs
+opt.shiftwidth = 4 --4 spaces for indent width
 opt.softtabstop = 4
-opt.expandtab = true  -- expand tab to spaces
+opt.expandtab = true -- expand tab to spaces
 opt.smartindent = true
 opt.autoindent = true -- copy indent from current line when starting new
 opt.wrap = false
-opt.linebreak = true  -- linebreak soft wrap at words
+opt.linebreak = true -- linebreak soft wrap at words
 
 -- Search
 opt.incsearch = true
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 opt.hlsearch = false
 
 -- Appearance
@@ -37,8 +36,8 @@ opt.number = true -- show the absolute number
 opt.relativenumber = true
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.colorcolumn = "100"
-opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
+opt.colorcolumn = "120"
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.cmdheight = 1
 opt.scrolloff = 10
 opt.completeopt = "menuone,noinsert,noselect"
