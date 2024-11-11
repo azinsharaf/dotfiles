67bc3b6744084bab8c6a61522794651c059fd53a
Azin Sharaf <azin.sharaf@icloud.com>
2024-11-11 09:30:21 -0800

    yazi, init lua

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,15 +1,15 @@
 function Linemode:size_and_mtime()
-    local time = math.floor(self._file.cha.modified or 0)
-    if time == 0 then
-        time = ""
-    elseif os.date("%Y", time) == os.date("%Y") then
-        time = os.date("%b %d %H:%M", time)
-    else
-        time = os.date("%b %d  %Y", time)
-    end
+	local time = math.floor(self._file.cha.modified or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
 
-    local size = self._file:size()
-    return ui.Line(string.format("%s %s", size and ya.readable_size(size) or "-", time))
+	local size = self._file:size()
+	return ui.Line(string.format("%s %s", size and ya.readable_size(size) or "-", time))
 end
 
 -- [plugins] - full-border
@@ -26,88 +26,97 @@ require("relative-motions"):setup({ show_numbers = "relative" })
 
 -- [plugin] - status-line
 require("yatline"):setup({
-    theme = "catppuccin_theme",
+	theme = "catppuccin_theme",
 
-    section_separator = { open = "", close = "" },
-    part_separator = { open = "", close = "" },
-    inverse_separator = { open = "", close = "" },
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
+	inverse_separator = { open = "", close = "" },
 
-    style_a = {
-        fg = "black",
-        bg_mode = {
-            normal = "#a89984",
-            select = "#d79921",
-            un_set = "#d65d0e",
-        },
-    },
-    style_b = { bg = "#665c54", fg = "#ebdbb2" },
-    style_c = { bg = "#3c3836", fg = "#a89984" },
+	style_a = {
+		fg = "black",
+		bg_mode = {
+			normal = "#a89984",
+			select = "#d79921",
+			un_set = "#d65d0e",
+		},
+	},
+	style_b = { bg = "#665c54", fg = "#ebdbb2" },
+	style_c = { bg = "#3c3836", fg = "#a89984" },
 
-    permissions_t_fg = "green",
-    permissions_r_fg = "yellow",
-    permissions_w_fg = "red",
-    permissions_x_fg = "cyan",
-    permissions_s_fg = "darkgray",
+	permissions_t_fg = "green",
+	permissions_r_fg = "yellow",
+	permissions_w_fg = "red",
+	permissions_x_fg = "cyan",
+	permissions_s_fg = "darkgray",
 
-    tab_width = 20,
-    tab_use_inverse = false,
+	tab_width = 20,
+	tab_use_inverse = false,
 
-    selected = { icon = "󰻭", fg = "yellow" },
-    copied = { icon = "", fg = "green" },
-    cut = { icon = "", fg = "red" },
+	selected = { icon = "󰻭", fg = "yellow" },
+	copied = { icon = "", fg = "green" },
+	cut = { icon = "", fg = "red" },
 
-    total = { icon = "󰮍", fg = "yellow" },
-    succ = { icon = "", fg = "green" },
-    fail = { icon = "", fg = "red" },
-    found = { icon = "󰮕", fg = "blue" },
-    processed = { icon = "󰐍", fg = "green" },
+	total = { icon = "󰮍", fg = "yellow" },
+	succ = { icon = "", fg = "green" },
+	fail = { icon = "", fg = "red" },
+	found = { icon = "󰮕", fg = "blue" },
+	processed = { icon = "󰐍", fg = "green" },
 
-    show_background = true,
+	show_background = true,
 
-    display_header_line = true,
-    display_status_line = true,
+	display_header_line = true,
+	display_status_line = true,
 
-    header_line = {
-        left = {
-            section_a = {
-            },
-            section_b = {
-            },
-            section_c = {
-            }
-        },
-        right = {
-            section_a = {
-            },
-            section_b = {
-            },
-            section_c = {
-            }
-        }
-    },
-    status_line = {
-        left = {
-            section_a = {
-                { type = "string", custom = false, name = "tab_mode" },
-            },
-            section_b = {
-                { type = "string", custom = false, name = "hovered_size" },
-            },
-            section_c = {
-                { type = "string", custom = false, name = "hovered_name" },
-            },
-        },
-        right = {
-            section_a = {
-                { type = "string", custom = false, name = "cursor_position" },
-            },
-            section_b = {
-                { type = "string", custom = false, name = "cursor_percentage" },
-            },
-            section_c = {
-                { type = "string", custom = false, name = "hovered_file_extension", params = { true } },
-                -- { type = "coloreds", custom = false, name = "permissions" },
-            },
-        },
-    },
+	header_line = {
+		left = {
+			section_a = {},
+			section_b = {},
+			section_c = {},
+		},
+		right = {
+			section_a = {},
+			section_b = {},
+			section_c = {},
+		},
+	},
+	status_line = {
+		left = {
+			section_a = {
+				{ type = "string", custom = false, name = "tab_mode" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "hovered_size" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_name" },
+			},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "cursor_position" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "cursor_percentage" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_file_extension", params = { true } },
+				-- { type = "coloreds", custom = false, name = "permissions" },
+			},
+		},
+	},
 })
+
+-- Show user/group of files in status bar
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+
+	return ui.Line({
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
+	})
+end, 500, Status.RIGHT)
