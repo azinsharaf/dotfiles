7b881bc871a21147d6bf28f7e7fbeb3579df5602
Azin Sharaf <azin.sharaf@icloud.com>
2024-11-12 09:32:22 -0800

    nvim, cmp plugin

--- dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,84 +1,84 @@
 return {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-        "hrsh7th/cmp-buffer",   -- source for text in buffer
-        "hrsh7th/cmp-path",     -- source for file system paths
-        {
-            "L3MON4D3/LuaSnip", -- snippet engine
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer", -- source for text in buffer
+		"hrsh7th/cmp-path", -- source for file system paths
+		{
+			"L3MON4D3/LuaSnip", -- snippet engine
 
-            -- follow latest release.
-            version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-            -- install jsregexp (optional!).
-            -- build = "make install_jsregexp",
-        },
-        "saadparwaiz1/cmp_luasnip",     -- for autocompletion
-        "rafamadriz/friendly-snippets", -- useful snippets
-        "onsails/lspkind.nvim",         -- vs-code like pictograms
-    },
-    config = function()
-        local cmp = require("cmp")
+			-- follow latest release.
+			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+			-- install jsregexp (optional!).
+			-- build = "make install_jsregexp",
+		},
+		"saadparwaiz1/cmp_luasnip", -- for autocompletion
+		"rafamadriz/friendly-snippets", -- useful snippets
+		"onsails/lspkind.nvim", -- vs-code like pictograms
+	},
+	config = function()
+		local cmp = require("cmp")
 
-        local luasnip = require("luasnip")
+		local luasnip = require("luasnip")
 
-        local lspkind = require("lspkind")
+		local lspkind = require("lspkind")
 
-        -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-        require("luasnip.loaders.from_vscode").lazy_load()
+		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-        cmp.setup({
-            completion = {
-                -- keyword_length = 2, -- Set the minimum keyword length to trigger completion
-                completeopt = "menu,menuone,preview,noselect",
-            },
-            snippet = { -- configure how nvim-cmp interacts with snippet engine
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        -- Select the next item, but if it's the first time, it'll select the first item
-                        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                    else
-                        fallback()  -- Fall back to the default Tab behavior (e.g., inserting a tab character)
-                    end
-                end, { 'i', 's' }), -- 'i' for insert mode, 's' for select mode
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item() -- Select the previous item in the completion list
-                    else
-                        fallback()             -- Fall back to the default Shift-Tab behavior
-                    end
-                end, { 'i', 's' }),
+		cmp.setup({
+			completion = {
+				keyword_length = 2, -- Set the minimum keyword length to trigger completion
+				completeopt = "menu,menuone,preview,noselect",
+			},
+			snippet = { -- configure how nvim-cmp interacts with snippet engine
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						-- Select the next item, but if it's the first time, it'll select the first item
+						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+					else
+						fallback() -- Fall back to the default Tab behavior (e.g., inserting a tab character)
+					end
+				end, { "i", "s" }), -- 'i' for insert mode, 's' for select mode
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item() -- Select the previous item in the completion list
+					else
+						fallback() -- Fall back to the default Shift-Tab behavior
+					end
+				end, { "i", "s" }),
 
-                ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-                ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-                ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
-                ["<CR>"] = cmp.mapping.confirm({ select = false }),
-            }),
-            -- sources for autocompletion
-            sources = cmp.config.sources({
-                -- { name = "codeium" }, -- codeium ai code completion
-                { name = "nvim_lsp" },
-                { name = "luasnip" }, -- snippets
-                { name = "buffer" },  -- text within current buffer
-                { name = "path" },    -- file system paths
-                -- { name = "cmdline" },
-            }),
-            -- configure lspkind for vs-code like pictograms in completion menu
-            formatting = {
-                format = lspkind.cmp_format({
-                    mode = "symbol_text",
-                    maxwidth = 50,
-                    ellipsis_char = "...",
-                    symbol_map = { Codeium = "", }
-                }),
-            },
-        })
-    end,
+				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<C-e>"] = cmp.mapping.abort(), -- close completion window
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+			}),
+			-- sources for autocompletion
+			sources = cmp.config.sources({
+				-- { name = "codeium" }, -- codeium ai code completion
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- snippets
+				{ name = "buffer" }, -- text within current buffer
+				{ name = "path" }, -- file system paths
+				-- { name = "cmdline" },
+			}),
+			-- configure lspkind for vs-code like pictograms in completion menu
+			formatting = {
+				format = lspkind.cmp_format({
+					mode = "symbol_text",
+					maxwidth = 50,
+					ellipsis_char = "...",
+					symbol_map = { Codeium = "" },
+				}),
+			},
+		})
+	end,
 }
