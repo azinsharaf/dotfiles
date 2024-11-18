7dfbcd78ade3b2dac14d2f1ff4f1a65c754919bb
Azin Sharaf <azin.sharaf@icloud.com>
2024-11-18 13:09:10 -0800

    nvim, obsidian plugin

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -1,5 +1,6 @@
 local Path = require("plenary.path")
 
+-- define workspaces
 local workspacePaths = {
     {
         name = "work",
@@ -45,44 +46,46 @@ return {
     dependencies = {
         -- Required.
         "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
 
         -- see below for full list of optional dependencies 👇
     },
-    opts = {
-        workspaces = workspaces,
 
-        -- see below for full list of options 👇
-        daily_notes = {
-            -- Optional, if you keep daily notes in a separate directory.
-            folder = daily_notes_folder,
-            -- Optional, if you want to change the date format for the ID of daily notes.
-            date_format = "%Y-%m-%d-%A",
-            -- Optional, if you want to change the date format of the default alias of daily notes.
-            -- alias_format = "%B %-d, %Y",
-            -- Optional, default tags to add to each new daily note created.
-            default_tags = { "daily-notes" },
-            -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-            template = "template_daily_note.md"
-        },
-        -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
-        completion = {
-            -- Set to false to disable completion.
-            nvim_cmp = true,
-            -- Trigger completion at 2 chars.
-            min_chars = 1,
-        },
-        -- Optional, for templates (see below).
-        templates = {
-            folder = "templates",
-            date_format = "%Y-%m-%d",
-            time_format = "%H:%M",
-            -- A map for custom variables, the key should be the variable and the value a function
-            substitutions = {},
-        },
+    config = function()
+        require("obsidian").setup({
+            workspaces = workspaces,
 
+            -- see below for full list of options 👇
+            daily_notes = {
+                -- Optional, if you keep daily notes in a separate directory.
+                folder = daily_notes_folder,
+                -- Optional, if you want to change the date format for the ID of daily notes.
+                date_format = "%Y-%m-%d-%A",
+                -- Optional, if you want to change the date format of the default alias of daily notes.
+                -- alias_format = "%B %-d, %Y",
+                -- Optional, default tags to add to each new daily note created.
+                default_tags = { "daily-notes" },
+                -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+                template = "template_daily_note.md"
+            },
+            -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+            completion = {
+                -- Set to false to disable completion.
+                nvim_cmp = true,
+                -- Trigger completion at 2 chars.
+                min_chars = 1,
+            },
+            -- Optional, for templates (see below).
+            templates = {
+                folder = "templates",
+                date_format = "%Y-%m-%d",
+                time_format = "%H:%M",
+                -- A map for custom variables, the key should be the variable and the value a function
+                substitutions = {},
+            },
+        })
+    end,
 
-
-    },
     keys = {
         { "<leader>oa", "<cmd>ObsidianOpen<cr>",        desc = "Open in Obsidian App" },
         { "<leader>on", "<cmd>ObsidianNew<cr>",         desc = "Obsidian New" },
