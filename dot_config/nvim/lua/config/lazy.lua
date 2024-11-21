480ad3e32d4c47cd8fda19d34a741645bb38ff3f
Azin Sharaf <azin.sharaf@icloud.com>
2024-11-20 23:42:38 -0800

    nvim, lazy cleanup for lsp

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -11,4 +11,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp-config" } })
+require("lazy").setup({ { import = "plugins" }, { import = "plugins.mason-lsp-config" } })
