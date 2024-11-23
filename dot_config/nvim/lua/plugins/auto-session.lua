0c1e4e0ff4290170eedd2152f101d5cd1097c638
Azin Sharaf <azin.sharaf@icloud.com>
2024-11-23 12:48:36 -0800

    refactor: nvim, autosession, turn off

--- dot_config/nvim/lua/plugins/auto-session.lua
+++ dot_config/nvim/lua/plugins/auto-session.lua
@@ -5,7 +5,7 @@ return {
 		local auto_session = require("auto-session")
 
 		auto_session.setup({
-			auto_restore = true,
+			auto_restore = false,
 			suppresssed_dirs = { "~/", "~/repos/", "~/Downloads", "~/Documents", "~/Desktop/" },
 		})
 
