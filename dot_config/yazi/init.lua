a07aaa25199743b28acdf0c2f93cc854376d657d
Azin Sharaf <azin.sharaf@icloud.com>
2024-11-28 13:53:15 -0800

    feat:yazi, upgrade to version 0.4

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,5 +1,5 @@
 function Linemode:size_and_mtime()
-	local time = math.floor(self._file.cha.modified or 0)
+	local time = math.floor(self._file.cha.mtime or 0)
 	if time == 0 then
 		time = ""
 	elseif os.date("%Y", time) == os.date("%Y") then
@@ -24,8 +24,6 @@ require("starship"):setup()
 -- [plugin] relative motion
 require("relative-motions"):setup({ show_numbers = "relative" })
 
--- [plugin] eza-preview
-require("eza-preview"):setup()
 
 -- [plugin] - status-line
 require("yatline"):setup({
