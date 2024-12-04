9b0a7e0040bc4b5c249d633340fe27508fbc22e5
Azin Sharaf <azin.sharaf@icloud.com>
2024-12-04 09:24:14 -0800

    feat: nvim, color scheme

--- dot_config/nvim/lua/config/highlights.lua
+++
@@ -1,7 +0,0 @@
--- Set the global background color
--- vim.cmd([[ highlight Normal guibg=rgba(0,0,0,0.9) ]])
-
--- Set the background for Telescope
--- vim.cmd([[highlight TelescopeNormal guibg=NONE ctermbg=NONE]])
-
--- Define any other custom highlights here
