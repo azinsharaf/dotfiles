40529ceb9d3bc3a6e41515c2fd34b60a015bf182
Azin Sharaf <azin.sharaf@icloud.com>
2024-12-03 21:27:46 -0800

    refactor: nvim, remove highlight

--- dot_config/nvim/lua/config/highlights.lua
+++ dot_config/nvim/lua/config/highlights.lua
@@ -1,5 +1,5 @@
 -- Set the global background color
-vim.cmd([[ highlight Normal guibg=rgba(0,0,0,0.9) ]])
+-- vim.cmd([[ highlight Normal guibg=rgba(0,0,0,0.9) ]])
 
 -- Set the background for Telescope
 -- vim.cmd([[highlight TelescopeNormal guibg=NONE ctermbg=NONE]])
