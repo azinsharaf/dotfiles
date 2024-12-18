80dbaa3a92d2c797b496cc55aeafaeb231475cf4
Azin Sharaf <azin.sharaf@icloud.com>
2024-12-17 17:20:12 -0800

    refactor: nvim, git-blame, changing key-maps

--- dot_config/nvim/lua/plugins/git-blame.lua
+++ dot_config/nvim/lua/plugins/git-blame.lua
@@ -7,7 +7,7 @@ return {
 	config = function()
 		-- Plugin options
 		require("gitblame").setup({
-			enabled = true, -- Enable the plugin
+			enabled = false, -- Enable the plugin
 			message_template = " <summary> • <date> • <author> • <<sha>>", -- Custom blame message format
 			date_format = "%m-%d-%Y %H:%M:%S", -- Date format for blame messages
 			virtual_text_column = 1, -- Starting column for virtual text
@@ -15,7 +15,7 @@ return {
 
 		-- Define keybindings for the plugin
 		vim.keymap.set("n", "<leader>go", ":GitBlameOpenFileURL<CR>", { desc = "Open file in browser" })
-		vim.keymap.set("n", "<leader>gt", ":GitBlameToggle<CR>", { desc = "Toggle Git blame" })
+		vim.keymap.set("n", "<leader>tb", ":GitBlameToggle<CR>", { desc = "Toggle Git blame" })
 
 		-- Adjust shell settings based on the OS
 		if vim.loop.os_uname().sysname == "Windows_NT" then
