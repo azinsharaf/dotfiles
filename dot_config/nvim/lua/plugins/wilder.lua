4f3287fc1748d887ea91b147f39515651a93e590
Azin Sharaf <azin.sharaf@icloud.com>
2024-12-18 15:24:50 -0800

    feat: nvim, wilder, popup menu

--- dot_config/nvim/lua/plugins/wilder.lua
+++ dot_config/nvim/lua/plugins/wilder.lua
@@ -1,19 +1,39 @@
 return {
-    "gelguy/wilder.nvim",
-    enabled = true,
-    config = function()
-        local wilder = require("wilder")
-        wilder.setup({ modes = { ":", "/", "?" } })
+	"gelguy/wilder.nvim",
+	enabled = true,
+	dependencies = { "roxma/nvim-yarp" },
+	config = function()
+		local wilder = require("wilder")
+		wilder.setup({ modes = { ":", "/", "?" } })
 
-        wilder.set_option("pipeline", {
-            wilder.branch(wilder.cmdline_pipeline(), wilder.search_pipeline()),
-        })
+		wilder.set_option("pipeline", {
+			wilder.branch(
+				wilder.cmdline_pipeline({
+					-- sets the language to use, 'vim' and 'python' are supported
+					language = "python",
+					-- 0 turns off fuzzy matching
+					-- 1 turns on fuzzy matching
+					-- 2 partial fuzzy matching (match does not have to begin with the same first letter)
+					fuzzy = 1,
+				}),
+				wilder.python_search_pipeline({
+					-- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
+					pattern = wilder.python_fuzzy_pattern(),
+					-- omit to get results in the order they appear in the buffer
+					sorter = wilder.python_difflib_sorter(),
+					-- can be set to 're2' for performance, requires pyre2 to be installed
+					-- see :h wilder#python_search() for more details
+					engine = "re",
+				})
+			),
+		})
 
-        wilder.set_option(
-            "renderer",
-            wilder.wildmenu_renderer({
-                highlighter = wilder.basic_highlighter(),
-            })
-        )
-    end,
+		wilder.set_option(
+			"renderer",
+			wilder.popupmenu_renderer({
+				highlighter = wilder.basic_highlighter(),
+			})
+		)
+	end,
+	build = ":UpdateRemotePlugins",
 }
