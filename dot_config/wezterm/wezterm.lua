b37368a72e757ddd56b72d7fd6db6e88e286ed6c
Azin Sharaf <azin.sharaf@icloud.com>
2024-12-19 00:34:13 -0800

    refactor: wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,7 +1,13 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+local act = wezterm.action
 
 -- Initialize the configuration table
 local config = wezterm.config_builder()
+
+-- workspace switcher plugin
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+-- workspace_switcher.apply_to_config(config)
 
 -- Set default program based on platform
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
@@ -12,6 +18,8 @@ end
 
 -- Appearance settings
 config.color_scheme = "Catppuccin Mocha"
+
+config.unix_domains = { { name = "unix" } }
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 12.0
@@ -27,12 +35,23 @@ config.window_background_opacity = 0.95
 config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
+config.tab_max_width = 32
+config.switch_to_last_active_tab_when_closing_tab = true
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.integrated_title_buttons = { "Hide", "Maximize", "Close" }
 config.integrated_title_button_style = "Windows"
 config.integrated_title_button_alignment = "Right"
 config.integrated_title_button_color = "Auto"
 
+config.colors = {
+	tab_bar = {
+		active_tab = {
+			-- I use a solarized dark theme; this gives a teal background to the active tab
+			fg_color = "#073642",
+			bg_color = "#2aa198",
+		},
+	},
+}
 config.default_workspace = "main"
 
 -- Dim inactive panes
@@ -43,61 +62,101 @@ config.inactive_pane_hsb = {
 
 -- config.disable_default_key_bindings = true
 
--- config.leader = { key = "A", mods = "CTRL", timeout_milliseconds = 1000 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 
 config.keys = {
 
 	-- Command Palette launcher
-	{ key = "Space", mods = "CTRL|SHIFT", action = wezterm.action.ShowLauncher },
-	{ key = "X", mods = "CTRL|SHIFT", action = wezterm.action.ActivateCopyMode },
+	{ key = "Space", mods = "LEADER", action = wezterm.action.ShowLauncher },
+	{ key = "x", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
 
 	-- Pane navigation
-	{ key = "H", mods = "CTRL|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-	{ key = "J", mods = "CTRL|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-	{ key = "K", mods = "CTRL|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-	{ key = "L", mods = "CTRL|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
 
 	-- Pane splitting
 	{
-		key = "S",
-		mods = "CTRL|SHIFT",
+		key = "|",
+		mods = "LEADER",
 		action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
 	},
 	{
-		key = "V",
-		mods = "CTRL|SHIFT",
+		key = "-",
+		mods = "LEADER",
 		action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
 	},
 
 	-- Pane resizing
-	{ key = "LeftArrow", mods = "CTRL|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 1 } }) },
-	{ key = "DownArrow", mods = "CTRL|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 1 } }) },
-	{ key = "UpArrow", mods = "CTRL|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 1 } }) },
-	{ key = "RightArrow", mods = "CTRL|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 1 } }) },
+	{ key = "LeftArrow", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+	{ key = "DownArrow", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+	{ key = "UpArrow", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+	{ key = "RightArrow", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
 
+	{
+		key = "f",
+		mods = "ALT",
+		action = wezterm.action.TogglePaneZoomState,
+	},
 	-- Copy and paste
-	{ key = "C", mods = "CTRL", action = wezterm.action({ CopyTo = "Clipboard" }) },
-	{ key = "V", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+	{ key = "c", mods = "CTRL", action = wezterm.action({ CopyTo = "Clipboard" }) },
+	{ key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
 
 	-- Tab management
-	{ key = "T", mods = "CTRL|SHIFT", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-	{ key = "W", mods = "CTRL|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = false } }) },
+	{ key = "t", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+	{ key = "w", mods = "LEADER", action = wezterm.action({ CloseCurrentTab = { confirm = false } }) },
+	{ key = "Tab", mods = "CTRL", action = wezterm.action.ActivateTabRelative(1) },
 
 	-- Quick tab switching
-	{ key = "1", mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 0 }) },
-	{ key = "2", mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 1 }) },
-	{ key = "3", mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 2 }) },
-	{ key = "4", mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 3 }) },
-	{ key = "5", mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 4 }) },
-	{ key = "6", mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 5 }) },
-	{ key = "7", mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 6 }) },
-	{ key = "8", mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 7 }) },
-	{ key = "9", mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 8 }) },
 
+	{
+		key = "n",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "p",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+	{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+	{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+	{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+	{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+	{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+
+	{
+		key = ",",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+
+	-- Show list of workspaces
+	{
+		key = "s",
+		mods = "LEADER",
+		-- action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+		action = workspace_switcher.switch_workspace(),
+	},
 	-- Launch external programs
 	{
-		key = "E",
-		mods = "CTRL|SHIFT",
+		key = "e",
+		mods = "LEADER",
 		action = wezterm.action({ SpawnCommandInNewTab = { args = { "explorer.exe" } } }),
 	},
 }
@@ -121,7 +180,7 @@ config.mouse_bindings = {
 	-- Window drag with Super or Ctrl + Shift + Left-click
 	{
 		event = { Drag = { streak = 1, button = "Left" } },
-		mods = "CTRL|SHIFT",
+		mods = "LEADER",
 		action = wezterm.action.StartWindowDrag,
 	},
 }
@@ -135,75 +194,5 @@ config.front_end = "WebGpu"
 
 -- Optional: Configure some additional GPU-related optimizations
 config.freetype_load_target = "Light" -- Font rendering optimization
-
--- Function to set up tabs on startup
--- wezterm.on("gui-startup", function(cmd)
---     local mux = wezterm.mux
---
---     -- Create the first tab and set its title to "Neovim"
---     local tab1, pane1, window = mux.spawn_window(cmd or {})
---
---     tab1:set_title("Neovim")
---
---     -- Split the first tab horizontally
---     local pane2 = pane1:split({
---         direction = "Right",
---         cwd = wezterm.home_dir
---     })
---
---     -- Create the second tab and set its title to ""
---     local tab2  = window:spawn_tab({
---         cwd = wezterm.home_dir,
---         args = { "yazi" }
---     })
---     tab2:set_title("Yazi")
---
---     -- Activate the first tab by default
---     window:activate_tab(tab1)
--- end)
---
---
---
---
-
-local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
-
-tabline.setup({
-	options = {
-		icons_enabled = true,
-		theme = "Catppuccin Mocha",
-		color_overrides = {},
-		section_separators = {
-			left = wezterm.nerdfonts.pl_left_hard_divider,
-			right = wezterm.nerdfonts.pl_right_hard_divider,
-		},
-		component_separators = {
-			left = wezterm.nerdfonts.pl_left_soft_divider,
-			right = wezterm.nerdfonts.pl_right_soft_divider,
-		},
-		tab_separators = {
-			left = wezterm.nerdfonts.pl_left_hard_divider,
-			right = wezterm.nerdfonts.pl_right_hard_divider,
-		},
-	},
-	sections = {
-		tabline_a = { "mode" },
-		tabline_b = { "workspace" },
-		tabline_c = { " " },
-		tab_active = {
-			"index",
-			{ "process", padding = 0 },
-			"/",
-			{ "cwd", padding = { left = 0, right = 1 } },
-			{ "zoomed", padding = 0 },
-		},
-		tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
-		tabline_x = { "ram", "cpu" },
-		tabline_y = { "datetime", "battery" },
-		tabline_z = { "hostname" },
-	},
-	extensions = {},
-})
-tabline.apply_to_config(config)
 
 return config
