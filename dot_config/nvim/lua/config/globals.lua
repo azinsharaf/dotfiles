c9e8b8739138b305417a4ad66e04342dbed381be
Azin Sharaf <azin.sharaf@icloud.com>
2025-01-22 12:17:18 -0800

    refactor: neovim, mason, lsp, etc

--- dot_config/nvim/lua/config/globals.lua
+++ dot_config/nvim/lua/config/globals.lua
@@ -10,8 +10,8 @@ local computer_name = vim.loop.os_gethostname()
 
 -- Define paths for each computer that has pynvim installed
 local python_paths = {
-	["Azin-Desktop"] = "C:/Users/User1/.pyenv/pyenv-win/versions/3.12.1/env-pynvim/Scripts/python.exe",
-	["WS-Oakland-001"] = "C:/Users/asharaf/.pyenv/pyenv-win/versions/3.12.1/env-pynvim/Scripts/python.exe",
+	["Azin-Desktop"] = "C:/Users/azin/.pyenv/pyenv-win/versions/3.12.8/env-pynvim/Scripts/python.exe",
+	["WS-Oakland-001"] = "C:/Users/asharaf/.pyenv/pyenv-win/versions/3.12.8/env-pynvim/Scripts/python.exe",
 	["Azin-Macbook"] = "~/Library/Python/3.12/bin/python3",
 }
 
--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -8,7 +8,25 @@ return {
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 		bigfile = { enabled = true, syntax = false },
-		dashboard = { enabled = true },
+		dashboard = {
+
+			formats = {
+				key = function(item)
+					return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
+				end,
+			},
+			sections = {
+				{ section = "header" },
+				{ title = "MRU", padding = 1 },
+				{ section = "recent_files", limit = 8, padding = 1 },
+				{ title = "MRU ", file = vim.fn.fnamemodify(".", ":~"), padding = 1 },
+				{ section = "recent_files", cwd = true, limit = 8, padding = 1 },
+				{ title = "Sessions", padding = 1 },
+				{ section = "projects", padding = 1 },
+				{ title = "Bookmarks", padding = 1 },
+				{ section = "keys" },
+			},
+		},
 		indent = { enabled = true },
 		input = { enabled = true },
 		notifier = { enabled = true },
