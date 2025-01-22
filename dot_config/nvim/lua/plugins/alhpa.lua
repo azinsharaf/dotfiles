144b3372ebca6f73a36f1542c3b5182abef71290
Azin Sharaf <azin.sharaf@icloud.com>
2025-01-22 14:19:35 -0800

    feat: neovim, alhpa and dashboard

--- dot_config/nvim/lua/plugins/alhpa.lua
+++ dot_config/nvim/lua/plugins/alhpa.lua
@@ -1,31 +1,83 @@
 return {
 	"goolord/alpha-nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	enabled = false,
+	event = "VimEnter",
 	config = function()
 		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.startify")
-
-		dashboard.section.header.val = {
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                     ]],
-			[[       ████ ██████           █████      ██                     ]],
-			[[      ███████████             █████                             ]],
-			[[      █████████ ███████████████████ ███   ███████████   ]],
-			[[     █████████  ███    █████████████ █████ ██████████████   ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
+		local dashboard = require("alpha.themes.dashboard")
+		_Gopts = {
+			position = "center",
+			hl = "Type",
+			wrap = "overflow",
 		}
 
+		local function load_random_header()
+			math.randomseed(os.time())
+			local header_folder = vim.fn.stdpath("config") .. "/lua/plugins/header_img/"
+			local files = vim.fn.globpath(header_folder, "*.lua", true, true)
+			if #files == 0 then
+				return nil
+			end
+
+			local random_file = files[math.random(#files)]
+			local separator = package.config:sub(1, 1)
+			local module_name = "plugins.header_img." .. random_file:match("([^" .. separator .. "]+)%.lua$")
+
+			package.loaded[module_name] = nil
+
+			local ok, module = pcall(require, module_name)
+			if ok and module.header then
+				return module.header
+			else
+				return nil
+			end
+		end
+
+		local function change_header()
+			local new_header = load_random_header()
+			if new_header then
+				dashboard.config.layout[2] = new_header
+				vim.cmd("AlphaRedraw") -- Przeładuj dashboard
+			else
+				print("No images inside header_img folder.")
+			end
+		end
+
+		-- Inicjalne załadowanie nagłówka
+		local header = load_random_header()
+		if header then
+			dashboard.config.layout[2] = header
+		else
+			print("No images inside header_img folder.")
+		end
+
+		dashboard.section.buttons.val = {
+
+			dashboard.button("<C-d>", "󱓧  Open daily-notes", ":ObsidianToday<CR>"),
+			dashboard.button("o", "󰄉  Recent files", ":Telescope oldfiles <CR>"),
+			dashboard.button("L", "󱐥  Lazy Plugin Manager", "<cmd>Lazy<CR>"),
+			dashboard.button("r", "  Repos", ":e $HOME/repos/<CR>"),
+			dashboard.button("d", "󱗼  Dotfiles", ":e $HOME/.local/share/chezmoi/<CR>"),
+			dashboard.button("w", "  Change header image", function()
+				change_header()
+			end),
+			dashboard.button("q", "Quit", ":qa<CR>"),
+		}
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LazyVimStarted",
+			desc = "Add Alpha dashboard footer",
+			once = true,
+			callback = function()
+				local stats = require("lazy").stats()
+				local ms = math.floor(stats.startuptime * 100 + 0.5) / 100
+				dashboard.section.footer.val =
+					{ " ", " ", " ", " Loaded " .. stats.count .. " plugins  in " .. ms .. " ms " }
+				dashboard.section.header.opts.hl = "DashboardFooter"
+				pcall(vim.cmd.AlphaRedraw)
+			end,
+		})
+
+		dashboard.opts.opts.noautocmd = true
 		alpha.setup(dashboard.opts)
 	end,
 }
