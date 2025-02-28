b54bb67d2b3d4b2fe1b519405143c7e7c3fcd029
Azin Sharaf <azin.sharaf@icloud.com>
2025-02-27 17:38:22 -0800

    yazi, adding size_and_mtime function

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -3,3 +3,18 @@ require("full-border"):setup()
 
 -- [plugin] - starsship
 require("starship"):setup()
+
+-- to get the size and modified time for linemode
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+end
