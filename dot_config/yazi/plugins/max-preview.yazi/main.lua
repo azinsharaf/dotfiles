66a0547f6a68f4faf739e050d1c63a804f291fbd
Azin Sharaf <azin.sharaf@icloud.com>
2025-03-04 15:19:34 -0800

    yazi

--- dot_config/yazi/plugins/max-preview.yazi/main.lua
+++ dot_config/yazi/plugins/max-preview.yazi/main.lua
@@ -2,18 +2,24 @@
 --- @sync entry
 
 local function entry(st)
+	ya.notify {
+		title = "Deprecated plugin",
+		content = "The `max-preview` plugin is deprecated, please use the new `toggle-pane` plugin instead: https://github.com/yazi-rs/plugins/tree/main/toggle-pane.yazi",
+		timeout = 10,
+		level = "warn",
+	}
+
 	if st.old then
 		Tab.layout, st.old = st.old, nil
 	else
 		st.old = Tab.layout
 		Tab.layout = function(self)
-			local all = MANAGER.ratio.parent + MANAGER.ratio.current
 			self._chunks = ui.Layout()
 				:direction(ui.Layout.HORIZONTAL)
 				:constraints({
-					ui.Constraint.Ratio(MANAGER.ratio.parent, all),
-					ui.Constraint.Ratio(MANAGER.ratio.current, all),
-					ui.Constraint.Length(1),
+					ui.Constraint.Percentage(0),
+					ui.Constraint.Percentage(0),
+					ui.Constraint.Percentage(100),
 				})
 				:split(self._area)
 		end
