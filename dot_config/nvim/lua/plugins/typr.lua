9268c4c53095020978523676db021e13c303e672
Azin Sharaf <azin.sharaf@icloud.com>
2025-03-14 16:30:38 -0700

    feat: Add key mappings for Flash functionality in Typr plugin configuration

--- dot_config/nvim/lua/plugins/typr.lua
+++ dot_config/nvim/lua/plugins/typr.lua
@@ -1,14 +1,24 @@
 return {
-    "nvzone/typr",
-    enabled = true,
-    dependencies = "nvzone/volt",
-    opts = {},
-    cmd = { "Typr", "TyprStats" },
-    config = function()
-        local keymap = vim.keymap
-
-        -- Ensure keymap is set correctly
-        keymap.set("n", "<leader>t", "<cmd>Typr<CR>", { desc = "Typr" })
-        keymap.set("n", "<leader>s", "<cmd>TyprStats<CR>", { desc = "Typr Stats" })
-    end,
+	"nvzone/typr",
+	enabled = true,
+	dependencies = "nvzone/volt",
+	opts = {},
+	cmd = { "Typr", "TyprStats" },
+	keys = {
+		{
+			"s",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"S",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
 }
