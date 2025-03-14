da4c058ecab8c1121de073790ad01e6d32f1fa0f
Azin Sharaf <azin.sharaf@icloud.com>
2025-03-14 16:32:06 -0700

    nvim, typr

--- dot_config/nvim/lua/plugins/typr.lua
+++ dot_config/nvim/lua/plugins/typr.lua
@@ -6,29 +6,13 @@ return {
 	cmd = { "Typr", "TyprStats" },
 	keys = {
 		{
-			"s",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").jump()
-			end,
-			desc = "Flash",
-		},
-		{
-			"S",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").treesitter()
-			end,
-			desc = "Flash Treesitter",
-		},
-		{
-			"<leader>t",
+			"<leader>tt",
 			mode = "n",
 			"<cmd>Typr<CR>",
 			desc = "Typr",
 		},
 		{
-			"<leader>s",
+			"<leader>ts",
 			mode = "n",
 			"<cmd>TyprStats<CR>",
 			desc = "Typr Stats",
