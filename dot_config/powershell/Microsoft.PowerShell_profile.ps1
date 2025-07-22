b1d35f117e593c547e1217afe55bf8c7c9d3b960
Azin Sharaf <azin.sharaf@icloud.com>
2025-07-21 17:55:02 -0700

    pwsh, change pyenv env

--- dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -248,7 +248,7 @@ $Env:PYENV_HOME = "$Env:USERPROFILE\.pyenv\pyenv-win\"
 $Env:PYENV_ROOT = "$Env:USERPROFILE\.pyenv\pyenv-win\"
 
 # Auto-activate venv on PowerShell launch
-$venvPath = "$Env:USERPROFILE\.pyenv\pyenv-win\versions\3.10.11\env-fcds-project-6\Scripts\Activate.ps1"
+$venvPath = "$Env:USERPROFILE\.pyenv\pyenv-win\versions\3.10.11\env-fcds-project-7\Scripts\Activate.ps1"
 
 if (Test-Path $venvPath) {
     & $venvPath
