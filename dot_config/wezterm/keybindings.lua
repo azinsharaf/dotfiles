39ebf174ee3e2e15baea71f4789a65a94d933a6a
Azin Sharaf <azin.sharaf@icloud.com>
2025-08-30 12:10:50 -0700

    feat: add keybindings and theme configuration for wezterm setup

---
+++ dot_config/wezterm/keybindings.lua
@@ -0,0 +1,37 @@
+local wezterm = require("wezterm")
+
+return {
+	leader = { key = "a", mods = "CTRL" },
+	keys = {
+		-- Use leader key for activating pane navigation
+		{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+		{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+		{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+		{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+
+		-- Split pane horizontally
+		{
+			key = "-",
+			mods = "LEADER",
+			action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+		},
+
+		-- Split pane vertically
+		{
+			key = "v",
+			mods = "CTRL|LEADER",
+			action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+		},
+
+		-- Move between panes
+		{ key = "h", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+		{ key = "j", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+		{ key = "k", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+		{ key = "l", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+
+		-- Copy and paste
+		{ key = "c", mods = "CTRL", action = wezterm.action({ CopyTo = "Clipboard" }) },
+		{ key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+		{ key = "v", mods = "CTRL|LEADER", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+	},
+}
