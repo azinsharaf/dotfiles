2ed80a013e3cfeb437b6d502594d9d264218811a
Azin Sharaf <azin.sharaf@icloud.com>
2025-09-02 09:22:22 -0700

    feat: enhance wezterm configuration with OS detection and settings

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,22 +1,41 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
-local config = wezterm.config_builder()
+local function detect_os()
+	local target = wezterm.target_triple
+	if target:find("windows") then
+		return "windows"
+	elseif target:find("darwin") then
+		return "macos"
+	end
+end
 
--- This is where you actually apply your config choices.
+if detect_os() == "macos" then
+	config.default_prog = { "/bin/zsh" }
+end
 
--- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
+local config = {
+	-- Import configurations from other files
+	keys = require("keybindings"),
+	font = require("fonts"),
+	color_scheme = require("colors"),
+	-- Appearance settings
+	hide_tab_bar_if_only_one_tab = true, -- Hide the tab bar if there's only one tab
+	tab_bar_at_bottom = true, -- Move the tab bar to the bottom
+	use_fancy_tab_bar = true, -- Enable fancy tab bar
+	window_decorations = "RESIZE", -- Remove the title bar
+	window_padding = {
+		left = 5,
+		right = 5,
+		top = 5,
+		bottom = 5,
+	},
+	initial_rows = 30,
+	initial_cols = 100,
+	enable_scroll_bar = true, -- Enable scroll bar
+}
 
--- or, changing the font size, font, and color scheme.
-config.font_size = 10
-config.color_scheme = "AdventureTime"
+if detect_os() == "windows" then
+	config.default_prog = { "pwsh" }
+end
 
--- Specify the font
-config.font = wezterm.font("JetBrains Mono Nerd Font")
-
--- Set PowerShell as the default shell
-config.default_prog = { "pwsh" }
 return config
