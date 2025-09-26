90038fe0a2ede8dba5317e88a31287fb83343a3d
Azin Sharaf <azin.sharaf@icloud.com>
2025-09-25 21:04:45 -0700

    chore: disable default wezterm bindings and remove custom keybindings

--- dot_config/wezterm/keybindings.lua
+++ dot_config/wezterm/keybindings.lua
@@ -1,21 +1,3 @@
 local wezterm = require("wezterm")
 
-return {
-	-- Example keybinding
-	{ key = "t", mods = "CTRL|SHIFT", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-
-	-- Split pane
-	{ key = "d", mods = "CTRL|SHIFT", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-	{ key = "D", mods = "CTRL|SHIFT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-
-	-- Move cursor between panes
-	{ key = "h", mods = "CTRL|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-	{ key = "j", mods = "CTRL|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-	{ key = "k", mods = "CTRL|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-	{ key = "l", mods = "CTRL|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-	{
-		key = "Enter",
-		mods = "ALT",
-		action = wezterm.action.DisableDefaultAssignment,
-	},
-}
+return {}
