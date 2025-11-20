929f7d5da60bc573c3e81da3d74476a84ff7b20d
Azin Sharaf <azin.sharaf@icloud.com>
2025-11-20 12:14:03 -0800

    nvim, fixing some issues in lsp and formatters

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,51 +1,50 @@
 return {
-    "williamboman/mason.nvim",
-    enabled = true,
-    event = "VeryLazy",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-        -- import mason
-        local mason = require("mason")
+	"mason-org/mason.nvim",
+	enabled = true,
+	event = { "User", "VeryLazy" },
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-        -- import mason-lspconfig
-        local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-        local mason_tool_installer = require("mason-tool-installer")
+		local mason_tool_installer = require("mason-tool-installer")
 
-        -- enable mason and configure icons
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-        })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-        mason_lspconfig.setup({
-            -- list of servers for mason to install
-            ensure_installed = {
-                "html",
-                "cssls",
-                "lua_ls",
-                "pyright",
-            },
-            automatic_installation = true,
-        })
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"html",
+				"cssls",
+				"lua_ls",
+				"pyright",
+			},
+		})
 
-        mason_tool_installer.setup({
-            ensure_installed = {
-                "prettier", -- prettier formatter
-                "stylua",   -- lua formatter
-                "isort",    -- python formatter
-                "black",    -- python formatter
-                "pylint",
-            },
-            automatic_installation = true,
-        })
-    end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"black",
+				"prettier",
+				"isort",
+				"stylua",
+			},
+			auto_update = false,
+			run_on_start = true,
+		})
+	end,
 }
