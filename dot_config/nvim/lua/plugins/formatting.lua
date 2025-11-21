30ae3ba8854196289506b1ef935b291cf472a224
Azin Sharaf <azin.sharaf@icloud.com>
2025-11-21 09:37:44 -0800

    nvim, auto format

--- dot_config/nvim/lua/plugins/formatting.lua
+++ dot_config/nvim/lua/plugins/formatting.lua
@@ -1,51 +1,51 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require("conform")
-        conform.setup({
-            formatters_by_ft = {
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                javascriptreact = { "prettier" },
-                typescriptreact = { "prettier" },
-                svelte = { "prettier" },
-                css = { "prettier" },
-                html = { "prettier" },
-                json = { "prettier" },
-                yaml = { "prettier" },
-                markdown = { "prettier" },
-                graphql = { "prettier" },
-                liquid = { "prettier" },
-                lua = { "stylua" },
-                python = { "isort", "black" },
-            },
-            format_on_save = {
-                lsp_format = "fallback",
-                timeout_ms = 5000,
-            },
-            format_after_save = {
-                lsp_format = "fallback",
-            },
-            log_level = vim.log.levels.ERROR,
-            notify_on_error = true,
-            notify_no_formatters = true,
-            formatters = {
-                isort = {
-                    command = "isort",
-                    args = {
-                        "-",
-                    },
-                },
-            },
-        })
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				liquid = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+			},
+			format_on_save = {
+				lsp_format = "fallback",
+				timeout_ms = 5000,
+			},
+			format_after_save = {
+				lsp_format = "fallback",
+			},
+			log_level = vim.log.levels.ERROR,
+			notify_on_error = true,
+			notify_no_formatters = true,
+			formatters = {
+				isort = {
+					command = "isort",
+					args = {
+						"-",
+					},
+				},
+			},
+		})
 
-        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 1000,
-            })
-        end, { desc = "Format file or range (in visual mode)" })
-    end,
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
