4cbc7f2e46b6957ea2758cf2af6d81f0aedbc0c6
Azin Sharaf <azin.sharaf@icloud.com>
2025-11-23 10:56:22 -0800

    nvim, obsidian plugin revisit

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -105,9 +105,42 @@ return {
 		-- Configure obsidian.nvim with the valid workspace
 		require("obsidian").setup({
 
+			---@class obsidian.config.StatuslineOpts
+			---
+			---@field format? string
+			---@field enabled? boolean
+			statusline = {
+				format = "{{backlinks}} backlinks  {{properties}} properties  {{words}} words  {{chars}} chars",
+				enabled = true,
+			},
+
 			workspaces = {
 				{ name = "personal_notes", path = get_vault_paths().personal_notes },
 				{ name = "work_notes", path = get_vault_paths().work_notes },
+			},
+
+			log_level = vim.log.levels.INFO,
+			note_id_func = require("obsidian.builtin").zettel_id,
+			wiki_link_func = require("obsidian.builtin").wiki_link_id_prefix,
+			markdown_link_func = require("obsidian.builtin").markdown_link,
+			preferred_link_style = "wiki",
+			open_notes_in = "current",
+
+			---@class obsidian.config.FrontmatterOpts
+			---
+			--- Whether to enable frontmatter, boolean for global on/off, or a function that takes filename and returns boolean.
+			---@field enabled? (fun(fname: string?): boolean)|boolean
+			---
+			--- Function to turn Note attributes into frontmatter.
+			---@field func? fun(note: obsidian.Note): table<string, any>
+			--- Function that is passed to table.sort to sort the properties, or a fixed order of properties.
+			---
+			--- List of string that sorts frontmatter properties, or a function that compares two values, set to vim.NIL/false to do no sorting
+			---@field sort? string[] | (fun(a: any, b: any): boolean) | vim.NIL | boolean
+			frontmatter = {
+				enabled = true,
+				func = require("obsidian.builtin").frontmatter,
+				sort = { "id", "aliases", "tags" },
 			},
 
 			-- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
@@ -123,6 +156,7 @@ return {
 				date_format = "%Y-%m-%d-%A",
 				default_tags = { "daily-notes" },
 				template = "template_daily_note.md",
+				workdays_only = false,
 			},
 
 			templates = {
@@ -137,13 +171,23 @@ return {
 			-- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
 			sort_by = "modified",
 			sort_reversed = true,
+			max_lines = 1000,
 
 			-- Optional, configure additional syntax highlighting / extmarks.
 			-- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
 			ui = {
-				enable = false, -- set to false to disable all additional syntax features
+				enable = true, -- set to false to disable all additional syntax features
+				ignore_conceal_warn = false,
 				update_debounce = 200, -- update delay after a text change (in milliseconds)
 				max_file_length = 5000, -- disable UI features for files with more than this many lines
+				checkboxes = {
+					[" "] = { char = "󰄱", hl_group = "obsidiantodo" },
+					["~"] = { char = "󰰱", hl_group = "obsidiantilde" },
+					["!"] = { char = "", hl_group = "obsidianimportant" },
+					[">"] = { char = "", hl_group = "obsidianrightarrow" },
+					["x"] = { char = "", hl_group = "obsidiandone" },
+				},
+
 				-- Use bullet marks for non-checkbox lists.
 				bullets = { char = "•", hl_group = "ObsidianBullet" },
 				external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
@@ -169,10 +213,60 @@ return {
 				},
 			},
 
+			---@class obsidian.config.AttachmentsOpts
+			---
+			---Default folder to save images to, relative to the vault root (/) or current dir (.), see https://github.com/obsidian-nvim/obsidian.nvim/wiki/Images#change-image-save-location
+			---@field img_folder? string
+			---
+			---Default name for pasted images
+			---@field img_name_func? fun(): string
+			---
+			---Default text to insert for pasted images
+			---@field img_text_func? fun(path: obsidian.Path): string
+			---
+			---Whether to confirm the paste or not. Defaults to true.
+			---@field confirm_img_paste? boolean
+			attachments = {
+				img_folder = "3 - References/attachments",
+				img_text_func = require("obsidian.builtin").img_text_func,
+				img_name_func = function()
+					return string.format("Pasted image %s", os.date("%Y%m%d%H%M%S"))
+				end,
+				confirm_img_paste = true,
+			},
+
+			---@class obsidian.config.FooterOpts
+			---
+			---@field enabled? boolean
+			---@field format? string
+			---@field hl_group? string
+			---@field separator? string|false Set false to disable separator; set an empty string to insert a blank line separator.
+			footer = {
+				enabled = true,
+				format = "{{backlinks}} backlinks  {{properties}} properties  {{words}} words  {{chars}} chars",
+				hl_group = "Comment",
+				separator = string.rep("-", 80),
+			},
+
+			---@class obsidian.config.CheckboxOpts
+			---
+			---@field enabled? boolean
+			---
+			---Order of checkbox state chars, e.g. { " ", "x" }
+			---@field order? string[]
+			---
+			---Whether to create new checkbox on paragraphs
+			---@field create_new? boolean
 			checkbox = {
 				enabled = true,
 				create_new = true,
 				order = { " ", "x", "~", "!", ">" },
+			},
+
+			---@class obsidian.config.CommentOpts
+			---@field enabled boolean
+			comment = {
+				enabled = false,
 			},
 		})
 	end,
