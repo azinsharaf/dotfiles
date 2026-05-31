09b5dcf8422475937cd4fdb2ebf8c0b9766ed525
Azin Sharaf <azin.sharaf@icloud.com>
2026-05-31 00:12:52 -0700

    nvim, comment deprecated section in obsidian

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -148,12 +148,12 @@ return {
 			},
 
 			-- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
-			completion = {
-				-- Set to false to disable completion.
-				nvim_cmp = true,
-				-- Trigger completion
-				min_chars = 1,
-			},
+			-- completion = {
+			-- 	-- Set to false to disable completion.
+			-- 	nvim_cmp = true,
+			-- 	-- Trigger completion
+			-- 	min_chars = 1,
+			-- },
 
 			daily_notes = {
 				folder = "5 - Daily Notes/" .. os.date("%Y") .. "/" .. os.date("%m-%B"),
