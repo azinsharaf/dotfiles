3ff2d453854568cd65dfaa2077e11f7b5a3f0b67
Azin Sharaf <azin.sharaf@icloud.com>
2026-07-03 23:09:15 -0700

    wezterm, font

--- dot_config/wezterm/fonts.lua
+++ dot_config/wezterm/fonts.lua
@@ -1,6 +1,6 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return wezterm.font_with_fallback({
 	{ family = "JetBrainsMono Nerd Font", weight = "Regular", italic = false },
-	{ family = "Atkinson Hyperlegible Mono", weight = "Regular" },
+	{ family = "AtkinsonMono Nerd Font", weight = "Regular" },
 })
