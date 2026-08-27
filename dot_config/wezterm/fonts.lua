cc79f7148b7b3d62a79f2ead045b511d0195737e
Azin Sharaf <azin.sharaf@icloud.com>
2026-08-27 10:13:49 -0700

    weezterm, testing ioskeleymono font

--- dot_config/wezterm/fonts.lua
+++ dot_config/wezterm/fonts.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 
 return wezterm.font_with_fallback({
+	{ family = "IoskeleyMono Nerd Font", weight = "Regular", italic = false },
 	{ family = "JetBrainsMono Nerd Font", weight = "Regular", italic = false },
-	{ family = "AtkinsonMono Nerd Font", weight = "Regular" },
 })
