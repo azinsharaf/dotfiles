d9ef8124a0706e56ba19bdc1634a38823dd176d1
Azin Sharaf <asharaf@woodrodgers.com>
2026-08-28 15:55:46 -0700

    font, clean up

--- dot_config/wezterm/fonts.lua
+++ dot_config/wezterm/fonts.lua
@@ -1,6 +1,5 @@
 local wezterm = require("wezterm")
 
 return wezterm.font_with_fallback({
-	{ family = "IoskeleyMono Nerd Font", weight = "Regular", italic = false },
 	{ family = "JetBrainsMono Nerd Font", weight = "Regular", italic = false },
 })
